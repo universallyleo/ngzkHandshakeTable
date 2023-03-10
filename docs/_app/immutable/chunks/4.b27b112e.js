654581af4fb7e5631b2af50529c00a73fcd82e8f
aaronkychan <omgleo@protonmail.com>
2023-03-11 05:40:54 +0900

    remove docs

--- docs/_app/immutable/chunks/4.b27b112e.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/bday-page.svelte.a8b8bd7b.js";export{t as component};
--- docs/_app/immutable/chunks/5.0bf92561.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/progress-page.svelte.e9eff3f0.js";export{t as component};
