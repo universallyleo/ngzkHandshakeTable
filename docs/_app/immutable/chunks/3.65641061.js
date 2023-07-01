3912e8a7066fdb7cfee60ffbff90689e179b0be2
universallyleo <universallyleo@users.noreply.github.com>
2023-07-01 21:08:27 +0000

    Github Action auto rebuild on 2023-07-01

--- docs/_app/immutable/chunks/3.65641061.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/about-page.svelte.e5f11ed8.js";export{t as component};
--- docs/_app/immutable/chunks/4.a3f4182d.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/bday-page.svelte.5d7d6fb8.js";export{t as component};
