31d43c9e37f151386ccd0a8a3911c47dc2eaf00f
universallyleo <universallyleo@users.noreply.github.com>
2023-08-17 05:08:25 +0000

    Auto DB update 2023-08-17

--- docs/_app/immutable/chunks/paths.40e96b4c.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_oecofx)==null?void 0:s.base)??"/ngzkHandshakeTable";var e;const o=((e=globalThis.__sveltekit_oecofx)==null?void 0:e.assets)??a;export{o as a,a as b};
