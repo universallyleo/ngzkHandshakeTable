d6059b01e4aa7b64b5dce06f46e6477783df0754
universallyleo <110659061+universallyleo@users.noreply.github.com>
2024-02-19 22:35:17 +0900

    Update scrape.py
    update for 35th single scraping

--- scrape.py
+++ scrape.py
@@ -19,8 +19,8 @@ headers = forTUNE_data["headers"]
 datafolder = "./src/lib/data/"
 
 SITEURL = "https://fortunemusic.jp"
-BASEURL = "https://fortunemusic.jp/nogizaka_202312/"
-CDNUM = 34
+BASEURL = "https://fortunemusic.jp/nogizaka_202403/"
+CDNUM = 35
 CDTYPE = "Single"
 
 NAMESDICT = {
