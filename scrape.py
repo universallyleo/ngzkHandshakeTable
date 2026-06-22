5513b7176de2e8d5afb3444e027dcd0af10193bb
aaronkychan <110659061+universallyleo@users.noreply.github.com>
2026-06-22 13:48:15 +0900

    42Signle

--- scrape.py
+++ scrape.py
@@ -33,8 +33,8 @@ headers = forTUNE_data["headers"]
 datafolder = "./src/lib/data/"
 
 SITEURL = "https://fortunemusic.jp"
-BASEURL = "https://fortunemusic.jp/nogizaka_202604/"
-CDNUM = 41
+BASEURL = "https://fortunemusic.jp/nogizaka_202607/"
+CDNUM = 42
 CDTYPE = "Single"
 
 NAMESDICT = {
